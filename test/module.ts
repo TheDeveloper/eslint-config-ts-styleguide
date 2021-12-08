2f56afbcba909333cf2e02e30bf7eb2aedb997ea
Geoff Wagstaff <geoffwagstaff@gmail.com>
2021-12-08 11:09:16 +0000

    module test

---
+++ test/module.ts
@@ -0,0 +1,7 @@
+export default function test1() {
+
+}
+
+export const test2 = 1;
+export const test3 = 1;
+export const test4 = 1;
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,7 @@
+import {
+  test1, test2, test3, test4
+} from './module';
+
 function test() {
 
 }
