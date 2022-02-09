04f48cec4e80d9896fdb280c877637a169fd37ca
Geoff Wagstaff <geoff@gosquared.com>
2022-02-09 16:07:12 +0000

    add rules

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    '@thedeveloper/eslint-config-styleguide'
-  ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'warn',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+
     'no-restricted-syntax': 'off',
     'no-void': 'off',
+
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
